d390ea6d1ab97e5bf203e02a26018bfe251fda04
JoaoGundersen <soykrom@github.com>
2020-03-13 17:51:09 +0000

    Input process almost done

---
+++ proj.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<vector <int>> processInput(vector<int> &grades) {
+    char trash;
+    int nodes, lines;
+
+    cin >> nodes >> trash >> lines;
+
+    vector<vector <int>> matrix(nodes, vector <int>(nodes));
+
+    grades.resize(nodes);
+    for(int i = 0; i < nodes; i++) {
+        cin >> grades[i];
+    }
+
+    int node, buddy;
+    for(int i = 0; i < lines; i++) {
+        cin >> node >> buddy;
+
+        matrix[node - 1][buddy - 1] = 1;
+    }
+
+    return matrix;
+}
+
+int main() {
+    vector<int> grades;
+
+    vector<vector <int>> matrix = processInput(grades);
+    
+
+    return 0;
+}
