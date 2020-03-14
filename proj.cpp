ac553262236a03dccdcb8c1fb70e8bbcc6e03668
JoaoGundersen <soykrom@github.com>
2020-03-14 19:35:46 +0000

    Working, but ugly

--- proj.cpp
+++ proj.cpp
@@ -4,79 +4,104 @@
 using namespace std;
 
 class Node {
-    public:
-        int _grade;
-        bool _visited = false;
-        vector<Node> _pointedTo;
+  public:
+    int _grade;
+    bool _visited = false;
+    vector<Node*> _pointedTo;
 
-        Node(){}
+    Node() {
+      _grade = 0;
 
-        Node(int grade) {
-            _grade = grade;
+    }
+
+    Node(int grade) {
+        _grade = grade;
+    }
+
+    void addLine(Node *node) {
+        _pointedTo.push_back(node);
+    }
+
+    void setGrade(int grade){
+      _grade = grade;
+    }
+
+    void spreadGrade(Node *node) {
+      node->_visited = true;
+      for(int i = 0; i < (int) node->_pointedTo.size(); i++) {
+        if(!node->_pointedTo.at(i)->_visited) {
+          node->_pointedTo.at(i)->setGrade(node->_grade);
+          node->_pointedTo.at(i)->spreadGrade(node->_pointedTo.at(i));
         }
+      }
 
-        void addLine(Node node) {
-            _pointedTo.push_back(node);
-        }
-
-        void setGrade(int grade){
-          _grade = grade;
-        }
-
-        void spreadGrade(Node node) {
-          node._visited = true;
-          for(int i = 0; i < (int) node._pointedTo.size(); i++) {
-              if(!node._pointedTo.at(i)._visited) {
-                  node._pointedTo.at(i).setGrade(node._grade);
-                  node._pointedTo.at(i).spreadGrade(_pointedTo.at(i));
-              }
-            }
-          return;
-        }
+      return;
+    }
 
 };
 
-vector<Node> processInput(){
+vector<Node*> processInput() {
   int nodes, paths, grade;
   int begin, end;
   char eater;
 
   cin >> nodes >> eater >> paths;
 
-  vector<Node> students(nodes);
+  vector<Node*> students(nodes);
 
-  for(int i = 0; i < nodes; i++){
+  for(int i = 0; i < nodes; i++) {
     cin >> grade;
-    int cont = 0;
-    while(grade < students.at(cont)._grade) {cont++; }
-    for(int j = students.size() - 1; j > cont; j--) {
-       students.at(j) = students.at(j - 1);
-     }
-     students.at(cont).setGrade(grade);
+
+    Node *new_node = (Node *) malloc(sizeof(Node)); 
+    
+    *new_node = Node(grade);
+
+    students.at(i) = new_node;
   }
 
   //for the paths
-  for(int i = 0; i < paths; i++){
+  for(int i = 0; i < paths; i++) {
     cin >> begin >> end;
-    students.at(end - 1).addLine(students.at(begin - 1));
+    students.at(end - 1)->addLine(students.at(begin - 1));
   }
 
-
   return students;
+
 }
 
-int main(){
+//FEIO
+Node* highestUnvisited(vector<Node*> students) {
+  int pos = -1, grade = 0;
 
-  //Tornar a merda dos students em ponteiros -> * <- this thing right here
-  vector<Node> students = processInput();
-  for(int i = 0; i < (int) students.size(); i++){
-    if(!students.at(i)._visited){
-      students.at(i).spreadGrade(students.at(i));
+  for(int i = 0; i < (int) students.size(); i++) {
+    if(students.at(i)->_grade > grade && !students.at(i)->_visited) {
+      pos = i;
+      grade = students.at(i)->_grade;
     }
   }
 
-  for(int i = 0; i < (int) students.size(); i++){
-    printf("%d\n", students.at(i)._grade);
+  if(pos == -1) return NULL;
+
+  return students.at(pos);
+}
+
+int main() {
+
+  //Tornar a merda dos students em ponteiros -> * <- this thing right here
+  vector<Node*> students = processInput();
+
+  Node *actual_node = highestUnvisited(students);
+
+  while(true) {
+    if(!actual_node) break;
+
+    actual_node->spreadGrade(actual_node);
+
+    actual_node = highestUnvisited(students);
+  }
+
+  for(int i = 0; i < (int) students.size(); i++) {
+    printf("%d\n", students.at(i)->_grade);
   }
 
   return 0;
