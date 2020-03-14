fa1fe67be15833194ead0f2cdcd4e7ee3d0d7656
ADEMA06 <afonso.mm.carvalho@gmail.com>
2020-03-14 01:09:35 +0000

    Fucking falta os students serem ponteiros

--- proj.cpp
+++ proj.cpp
@@ -3,41 +3,82 @@
 
 using namespace std;
 
+class Node {
+    public:
+        int _grade;
+        bool _visited = false;
+        vector<Node> _pointedTo;
 
-vector<vector <int>> processInput(vector<int> &grades) {
-    char trash;
-    int nodes, lines;
+        Node(){}
 
-    cin >> nodes >> trash >> lines;
+        Node(int grade) {
+            _grade = grade;
+        }
 
-    vector<vector <int>> matrix(nodes, vector <int>(nodes));
+        void addLine(Node node) {
+            _pointedTo.push_back(node);
+        }
 
-    grades.resize(nodes);
-    for(int i = 0; i < nodes; i++) {
-        cin >> grades[i];
-    }
+        void setGrade(int grade){
+          _grade = grade;
+        }
 
-    int node, buddy;
-    for(int i = 0; i < lines; i++) {
-        cin >> node >> buddy;
+        void spreadGrade(Node node) {
+          node._visited = true;
+          for(int i = 0; i < (int) node._pointedTo.size(); i++) {
+              if(!node._pointedTo.at(i)._visited) {
+                  node._pointedTo.at(i).setGrade(node._grade);
+                  node._pointedTo.at(i).spreadGrade(_pointedTo.at(i));
+              }
+            }
+          return;
+        }
 
-        matrix[node - 1][buddy - 1] = 1;
-    }
+};
 
-    return matrix;
+vector<Node> processInput(){
+  int nodes, paths, grade;
+  int begin, end;
+  char eater;
+
+  cin >> nodes >> eater >> paths;
+
+  vector<Node> students(nodes);
+
+  for(int i = 0; i < nodes; i++){
+    cin >> grade;
+    int cont = 0;
+    while(grade < students.at(cont)._grade) {cont++; }
+    for(int j = students.size() - 1; j > cont; j--) {
+       students.at(j) = students.at(j - 1);
+     }
+     students.at(cont).setGrade(grade);
+  }
+
+  //for the paths
+  for(int i = 0; i < paths; i++){
+    cin >> begin >> end;
+    students.at(end - 1).addLine(students.at(begin - 1));
+  }
+
+
+  return students;
 }
 
-int main() {
-    vector<int> grades;
+int main(){
 
-    vector<vector <int>> matrix = processInput(grades);
-    
-
-    
-
-    for(int i = 0; i < grades.size(); i++) {
-        cout << grades.at(i) << "\n";
+  //Tornar a merda dos students em ponteiros -> * <- this thing right here
+  vector<Node> students = processInput();
+  for(int i = 0; i < (int) students.size(); i++){
+    if(!students.at(i)._visited){
+      students.at(i).spreadGrade(students.at(i));
     }
+  }
 
-    return 0;
+  for(int i = 0; i < (int) students.size(); i++){
+    printf("%d\n", students.at(i)._grade);
+  }
+
+  return 0;
+
 }
